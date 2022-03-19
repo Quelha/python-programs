7b521af21b04a32d5816bc4aaebae2de407b5686
Felipe Quelha <felipequelha@gmail.com>
2022-03-19 11:56:29 -0300

    importacao inicial

---
+++ NotaTecnica.py
@@ -0,0 +1,17 @@
+from pydoc import importfile
+
+
+import  re
+
+nt = 'Janeiro na data de 17/01/2022, valor de R$ 11.000,00'
+
+mes = input('Mes referente: ')
+data = input('Data referente: ')
+valor = input('Valor referente: ')
+
+if mes == 'Fevereiro':
+    mesAnt = 'Janeiro'
+
+
+
+print(nt)
