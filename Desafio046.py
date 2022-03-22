f0198dd5388feaecdf56da5a6b7075eb7b225b1e
Quelha <felipequelha@gmail.com>
2022-03-22 15:26:55 +0000

    finalizado

--- Desafio046.py
+++ Desafio046.py
@@ -1,4 +1,6 @@
 from time import sleep
+import webbrowser
+
 
 
 print('-='*18)
@@ -13,20 +15,43 @@ if status == 'Y':
     print('Queria de contar uma coisa muito importante')
     sleep(4)
     print('É que...')
-    sleep(5)
+    sleep(4)
     print('Então...')
-    sleep(6)
+    sleep(4)
     print('...')
     sleep(4)
     nada = input('Você sabe que eu te amo, né? [Y] SIM [Y] SIM ')
+    sleep(1)
     print('Ah que bom, muito importante você saber disso antes de te contar')
     sleep(5)
     x = input('Está sentada? [Y] SIM [N] NÃO ').upper()
     if x == 'Y':
-        sleep(5)
+        sleep(3)
         print('Ótimo, então acho que agora posso te contar')
     else:
-        sleep(5)
+        sleep(3)
         print('Melhor sentar então... :)')
+    sleep (4)
+    print('Não sou muito bom com textos mas vou deixar aqui algumas coisas que ja falei mas vou continar falando sempre')
+    sleep(9)
+    print('Todos os dias me sinto a pessoa mais sortuda por ter você comigo, e para estar do lado de alguem tão incrivel como você, a pessoa precisa ser MUITO sortuda')
+    sleep(13)
+    print('Você alem de LINDA, possui um caração gigante')
+    sleep(5)
+    print('Com uma beleza ABSURDA, interna e externa')
+    sleep(5)
+    print('Me apaixono por você cada dia mais')
+    sleep(4)
+    for i in range(1,5):
+        print('E mais')
+        sleep(1)
+    print('Espero poder fazer de você a mulher mais feliz de todas!')
+    sleep(5)
+    print('Beijo amor, espero que seu dia seja incrivel!')
+    sleep(4)
+    print('TE AMO S2')
+    fim = input(' ')
 else:
-    print('Poxa, o que houve??\nClica aqui nesse link e me conta o que está acontecendo')
+    print('Poxa, o que houve??\nAperta ENTER e me conta o que está acontecendo')
+    enter = input('')
+    webbrowser.open('https://wa.me/5521965550516?text=Amor%20quero%20conversar')
--- NotaTecnica.py
+++ NotaTecnica.py
@@ -1,10 +1,10 @@
 
 nt = """ASSUNTO:
-Trata esta nota da avaliação específica da prestação de serviços de TELEMAR NORTE LESTE S/A, disponibilizados através do Contrato 08/2018, para o mês de Janeiro/2022.\n\n"""
+Trata esta nota da avaliação específica da prestação de serviços de TELEMAR NORTE LESTE S/A, disponibilizados através do Contrato 08/2018, para o mês de Fevereiro/2022.\n\n"""
 nt2 = """FATOS:
-Em 17/01/2022 foi disponibilizada a fatura de telefonia fixa código 350 4329 no valor de R$ 11.051,48 (28190344) e no dia 20/01/2022 foi disponibilizada a fatura da telefonia 0800 código 350 5110, no valor de R$ 1.123,51 (28190786), competência Janeiro 2022.
-Foi inserido relatório BI com detalhes das ligações realizadas em Janeiro/2022, no processo de fiscalização mensal SEI-040161/003086/2021
- 
+Em 15/02/2022 foi disponibilizada a fatura de telefonia fixa código 350 4329 no valor de R$ 11.516,01 (29357128) e no dia 21/02/2022 foi disponibilizada a fatura da telefonia 0800 código 350 5110, no valor de R$ 1.938,90 (29357022), competência Fevereiro 2022.
+Foi inserido relatório BI com detalhes das ligações realizadas em Fevereiro/2022, no processo de fiscalização mensal SEI-040161/003086/2021
+
 CONCLUSÃO:
 A empresa atendeu satisfatoriamente aos parâmetros técnicos relativos à prestação de serviço de telefonia fixa, demonstrando ter capacidade técnica para prestar os serviços de telefonia comum e 0800 ao Rioprevidência."""
 
@@ -28,7 +28,7 @@ dict = {
 }
 
 '''SUBSTITUINDO E JUNTANDO OS DOIS TEXTOS'''
-nt = nt.replace(dict[mesAnt],dict[mes]) + nt2.replace(nt2[10:20],dataComum).replace(nt2[129:138],data0800).replace(nt2[99:109], valorComum).replace(nt2[219:227], valor0800).replace(dict[mesAnt],dict[mes])
+nt = nt.replace(dict[mesAnt],'\033[1m' + dict[mes] + '\033[0m') + nt2.replace(nt2[10:20],dataComum).replace(nt2[129:139],data0800).replace(nt2[99:109], valorComum).replace(nt2[219:227], valor0800).replace(dict[mesAnt],dict[mes])
 
 print(nt)
 
